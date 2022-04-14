d4c4c8c23a5fbb92e6b548a42dc18f82c7a4f3ee
John Dingee <dingeej@gmail.com>
2022-04-14 11:16:57 -0400

    Feature/fe (#3)
    * create react typescript app
    
    * create a react app and load data into it
    
    * sketchily serve data to the fe
    
    * fe working better
    
    * remove the search box

---
+++ fe/src/App.js
@@ -0,0 +1,46 @@
+import logo from './logo.svg';
+import './App.css';
+
+// import ReactSearchBox from "react-search-box";
+
+
+import GraphViz from './GraphViz'
+
+
+function App() {
+  const data = [
+    {
+      key: "john",
+      value: "John Doe",
+    },
+    {
+      key: "jane",
+      value: "Jane Doe",
+    },
+    {
+      key: "mary",
+      value: "Mary Phillips",
+    },
+    {
+      key: "robert",
+      value: "Robert",
+    },
+    {
+      key: "karius",
+      value: "Karius",
+    },
+  ]
+
+  return ([
+    // <ReactSearchBox
+    //    placeholder="Placeholder"
+    //    value="Doe"
+    //    data={data}
+    //    callback={(record) => console.log(record)}
+    //  />,
+    <GraphViz/>
+  ]
+  );
+}
+
+export default App;
